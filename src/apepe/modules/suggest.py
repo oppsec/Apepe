536547241afd4d19039f0001c196b96d7ddf9270
oppsec <opps3c@gmail.com>
2023-06-24 16:32:45 -0300

    new: added option to list ssl pinnings scripts

---
+++ src/apepe/modules/suggest.py
@@ -0,0 +1,19 @@
+from rich import print
+
+urls = [
+    ['https://github.com/horangi-cyops/flutter-ssl-pinning-bypass', 'Flutter'],
+    ['https://gist.github.com/owen800q/9c5ee9392f45dbcf44c5b434cdecf647', 'Flutter'],
+    ['https://codeshare.frida.re/@vadim-a-yegorov/universalunpinner/', 'Java'],
+    ['https://codeshare.frida.re/@vadim-a-yegorov/universalunpinner/', 'React'],
+    ['https://codeshare.frida.re/@vadim-a-yegorov/universalunpinner/', 'Flutter'],
+]
+
+def suggest_sslpinning(lang) -> str:
+    ''' This function will pass through the urls array values and return they '''
+
+    for url in urls:
+        script_url: str = url[0]
+        script_lang: str = url[1]
+
+        if(lang == script_lang):
+            return f'[yellow][i]* SSL pinning script recommend: [u]{script_url}[/i][/u][/]'
